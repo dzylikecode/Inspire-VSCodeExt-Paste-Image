6f1e500f4238f819e7c8c63f8ca03ce391bf293d
Dz <MathBeWithCode@gmail.com>
2023-01-20 19:38:09 +0800

    full path of powershell

--- src/clipboard/clipboard.js
+++ src/clipboard/clipboard.js
@@ -4,12 +4,14 @@ const vscode = require("vscode");
 const fs = require("fs");
 const config = require("../config.js");
 const isWsl = require("is-wsl");
+let powershellWSL = "";
 function Clipboard() {
   const platform = process.platform;
   if (platform == "win32") {
     this.isImage = isImageWin;
     this.saveImageCore = saveImageWin;
   } else if (isWsl) {
+    powershellWSL = checkPowershellWSL();
     this.isImage = isImageWSL;
     this.saveImageCore = saveImageWSL;
   }
@@ -44,7 +46,7 @@ async function isImageWin() {
 
 // 由于执行速度的问题, 将脚本压缩为命令行形式
 async function isImageWSL() {
-  let powershell = spawn("powershell.exe", [
+  let powershell = spawn(powershellWSL, [
     "-noprofile",
     "-command",
     "if(Get-Clipboard -Format Image){1}else{0}",
@@ -77,7 +79,7 @@ function saveImageWSL(fileDir, fileName) {
   // powershell 不能识别wsl的路径, 所以先转到ps1脚本工作, 然后移动文件到目标文件夹下
   return new Promise((resolve, reject) => {
     spawn(
-      "powershell.exe",
+      powershellWSL,
       [
         "-noprofile",
         "-command",
@@ -94,6 +96,11 @@ function saveImageWSL(fileDir, fileName) {
         }).on("close", resolve)
       );
   });
+}
+
+function checkPowershellWSL() {
+  let command = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe";
+  return fs.existsSync(command) ? command : "powershell.exe";
 }
 
 module.exports = new Clipboard();
