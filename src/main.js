c8f3e869f61df3cb7aff4aba111b28990e52c22c
Dz <MathBeWithCode@gmail.com>
2023-02-26 01:35:19 +0800

    add compress function

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const config = require("./config.js");
 const clipboard = require("./clipboard");
 const getFullPath = require("./getFullPath.js");
 const render = require("./render.js");
+const compress = require("./compress.js");
+const logger = require("./logger.js");
+const path = require("path");
 
 async function main(context) {
   config.init(context);
@@ -29,7 +32,28 @@ async function main(context) {
 async function pasteImage() {
   const filePath = await getFullPath(config.confirmPattern);
   render(config.baseDir, filePath); // 先渲染出来
-  await clipboard.saveImage(filePath);
+  const savePath = await clipboard.saveImage(filePath);
+  if (config.compressEnable) {
+    const fileSize = compress.getFilesizeInBytes(savePath);
+    if (fileSize > config.compressThreshold) {
+      compressFile(path.dirname(savePath), savePath);
+    }
+  }
+}
+
+async function compressFile(destDir, sourceFile) {
+  let fileSize = compress.getFilesizeInBytes(sourceFile);
+  logger.log(sourceFile);
+  logger.log(`before fileSize: ${fileSize}`);
+  try {
+    await compress.compressIMG(destDir, [sourceFile]);
+  } catch (e) {
+    logger.log(e);
+  }
+  let fileName = path.basename(sourceFile);
+  let destFile = path.join(destDir, fileName);
+  fileSize = compress.getFilesizeInBytes(destFile);
+  logger.log(`after fileSize: ${fileSize}`);
 }
 
 module.exports = {
